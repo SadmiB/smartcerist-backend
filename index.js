1486a07ed7b826ce49297759b3d87113e482b83a
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-04-02 10:46:30 +0100

    added authentication

--- index.js
+++ index.js
@@ -8,13 +8,26 @@ import notificationsRoutes from './src/routes/notificationsRoutes';
 import objectsRoutes from './src/routes/objectsRoutes';
 import serversRoutes from './src/routes/serversRoutes';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import { UserSchema } from './src/models/usersModel';
 
 var app = express();
+const User = mongoose.model('User', UserSchema)
+
 
 // json parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 const PORT = 3000;
+
+
+// to allow angular client
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 // mongodb connection
 mongoose.Promise = global.Promise;
@@ -33,6 +46,47 @@ app.use(express.static('public'));
 app.get('/', (req, res) => 
     res.send(`server is running in port: ${PORT}`)
 );
+
+app.post('/signup', async (req, res) => {
+    console.log('signup...');
+    let newUser = new User(req.body)
+    let user = await newUser.save();
+    sendToken(user, res);
+});
+
+app.post('/signin', async (req, res) => {
+    console.log('signin...');
+    var user = await User.findOne({email: req.body.email});
+    
+    if(!user) 
+        sendAuthError(res);
+    if (user.password == req.body.password)
+        sendToken(user, res);
+    else
+        sendAuthError(res);
+});
+
+function sendToken(user, res) {
+    var token = jwt.sign(user.id, '123'); // in production dont hard code the second argument
+    res.json({firstName: user.firstName, token: token});
+}
+
+
+function sendAuthError(res) {
+    return res.json({success: false, message: 'email or password incorrect'});
+}
+
+function checkAuthenticated(req, res, next) {
+    if(!req.header('authorization'))
+        return res.status(401).send({message: 'Unauthorized request. Missing authenication header'} );
+    var token = req.header('authorization').split(' ')[1];
+    var payload = jwt.decode(token, '123');
+    if(!payload)
+        res.status(401).send({message: 'Unauthorized request. Authentication header invalid'});
+    
+    req.user = payload;
+    next();
+}
 
 var server = app.listen(PORT, () =>
     console.log(`server is running in port: ${PORT}`)
